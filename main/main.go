296a94f8c0d76529391f08e8149639f89c593eb5
Peter J Coles <peterjohncoles@gmail.com>
2022-10-16 13:01:37 +0100

    Just commiting

--- main/main.go
+++ main/main.go
@@ -7,18 +7,29 @@ import (
 
 func main() {
 	s := tetris.ReadFile()
-	a := [][]string{}
+	a := []string{}
+	b := []string{}
+	str := ""
+	r := 30
 	for i := 0; i < len(s); i++ {
 		if len(s[i]) > 0 {
-			a = append(a, []string{s[i]})
+			a = append(a, string(s[i]))
 		}
 	}
-
-	for j := 0; j < len(a); j++ {
-		for k := 0; k < len(a[j]); k++ {
-			for l := 0; l < len(a[j][k]); l++ {
-				fmt.Println(a[j][k][l])
+	fmt.Println(a)
+	for row := 0; row < len(a); row++ {
+		for col := 0; col < len(a[row]); col++ {
+			if col == len(a[row])-1 || row == len(a)-1 {
+				fmt.Println(str)
+				b = append(b, str)
+				str = ""
+			}
+			if a[row][col] == '#' {
+				str += string(int(a[row][col]) + r)
 			}
 		}
+	}
+	for n := 0; n < len(b); n++ {
+		fmt.Println([]byte(b[n]))
 	}
 }
